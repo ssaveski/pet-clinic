c71b47490deaac1ed250e99e892c4d23a51c248a
ssaveski <saveskistef@gmai.l.com>
2020-03-31 17:50:12 +0200

    Implement Interfaces for models, Closes #5

---
+++ pet-clinic-data/src/main/java/com/saveski/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.saveski.petclinic.services;
+
+import com.saveski.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
