c5d02123e44eb2cd02fe87550be6667b5be99d64
ssaveski <saveskistef@gmai.l.com>
2020-03-31 19:23:12 +0200

    Crud Interface

--- pet-clinic-data/src/main/java/com/saveski/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/saveski/petclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.saveski.petclinic.services;
 
 import com.saveski.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
