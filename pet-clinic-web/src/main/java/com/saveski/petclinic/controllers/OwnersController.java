c0de9057f44c37fbbcaa800d0b22d3896012fa73
ssaveski <saveskistef@gmai.l.com>
2020-04-03 22:51:45 +0200

    Fixed broken links

--- pet-clinic-web/src/main/java/com/saveski/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/com/saveski/petclinic/controllers/OwnersController.java
@@ -15,10 +15,15 @@ public class OwnersController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/", "/index", "/index.html"})
+    @RequestMapping({"", "/", "/index", "/index.html"})
     public String listOwners(Model model) {
 
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
+    }
+
+    @RequestMapping({"/find"})
+    public String findOwners() {
+        return "notImplemented";
     }
 }
