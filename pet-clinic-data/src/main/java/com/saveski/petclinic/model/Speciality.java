ec66c73cc1edb5b5824730918518bcca7ea49a76
ssaveski <saveskistef@gmai.l.com>
2020-04-03 21:28:31 +0200

    Speciality Map Implementation, Closes #29

--- pet-clinic-data/src/main/java/com/saveski/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/saveski/petclinic/model/Speciality.java
@@ -1,6 +1,6 @@
 package com.saveski.petclinic.model;
 
-public class Speciality {
+public class Speciality extends BaseEntity {
 
     private String description;
 
---
+++ pet-clinic-data/src/main/java/com/saveski/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package com.saveski.petclinic.services;
+
+import com.saveski.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
